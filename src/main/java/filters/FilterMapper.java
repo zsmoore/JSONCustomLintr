c1fb061bd94e8b521f8ac6cc8e79275ca0852d7e
zsmoore <zsmoore@zachary-moore.com>
2019-01-13 15:48:23 -0800

    Fix gradle build optimize imports

--- src/main/java/filters/FilterMapper.java
+++ src/main/java/filters/FilterMapper.java
@@ -4,9 +4,7 @@ import lint.LintImplementation;
 import objects.JSONArray;
 import objects.JSONFile;
 import objects.JSONObject;
-import objects.WrappedObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FilterMapper {
@@ -14,7 +12,6 @@ public class FilterMapper {
     //@SuppressWarnings("unchecked")
     public static List<?> filter(JSONFile jsonFile, LintImplementation lintImplementation) {
         Filters filters = new Filters();
-        System.out.println(lintImplementation.getClazz());
         if (lintImplementation.getClazz() == String.class) {
             return filters.filterToStrings(jsonFile);
         } else if (lintImplementation.getClazz() == JSONObject.class) {
