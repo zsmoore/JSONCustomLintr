46271c9555bd7950d118815fc94b602146460558
zsmoore <zsmoore@zachary-moore.com>
2019-01-13 03:21:30 -0800

    Add filters to be able to filter parse output to certain types

--- src/test/java/learn/MainTest.java
+++ src/test/java/learn/MainTest.java
@@ -1,10 +1,14 @@
 package learn;
 
+import detectors.Filters;
+import objects.JSONArray;
 import objects.JSONFile;
+import objects.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class MainTest {
@@ -12,18 +16,37 @@ public class MainTest {
     private JSONFile jsonFile;
     private static Logger log;
 
-
     @Before
     public void setup() throws Exception {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("test-file.pdsc").getFile());
-        jsonFile = new JSONFile(file);
-        log = Logger.getLogger("Logger");
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("test-file.pdsc").getFile());
+//        jsonFile = new JSONFile(file);
+//        log = Logger.getLogger("Logger");
     }
 
     @Test
     public void testJson() throws Exception {
-        jsonFile.getObject().toMap().entrySet().forEach(entry ->
-            log.info(entry.getKey() + "\t" + entry.getValue()));
+        Filters filter = new Filters();
+//        log.info(filter.filterToObjects(jsonFile).toString());
+//        log.info(filter.filterToArrays(jsonFile).toString());
+//        log.info(filter.filterToStrings(jsonFile).toString());
+
+//        ArrayList<WrappedPrimitive<String>> wrappedStrings = filter.filterToStrings(jsonFile);
+//        wrappedStrings.forEach(e-> log.info(e.getOriginatingKey() + "\t" + e.toString()));
+
+//        ArrayList<JSONObject> objects = filter.filterToObjects(jsonFile);
+//        objects.forEach(e -> log.info(e.getOriginatingKey()));
+//
+//        objects.forEach(jsonObject -> {
+//            if (jsonObject.getOriginatingKey().equals("fields")
+//                    && jsonObject.getParentObject() instanceof JSONArray
+//                    && jsonObject.get("type").equals("boolean")
+//                    && ((String) jsonObject.get("name")).startsWith("has")) {
+//                log.info("CAUGHT");
+//            }
+//        });
+
+//        jsonFile.getObject().toMap().entrySet().forEach(entry ->
+//            log.info(entry.getKey() + "\t" + entry.getValue()));
     }
 }
