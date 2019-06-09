dc84965f3039b3cf3f55a088a0894f5e822f78a4
zsmoore <zsmoore@zachary-moore.com>
2019-06-08 17:35:42 -0700

    Adding ability to get line number of error

--- src/main/java/com/zachary_moore/objects/JSONFile.java
+++ src/main/java/com/zachary_moore/objects/JSONFile.java
@@ -7,15 +7,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JSONFile {
 
     private org.json.JSONObject jsonObject;
     private org.json.JSONArray jsonArray;
     private String filePath;
+    private List<String> linesInFile;
 
     public JSONFile(File file) throws IOException {
         filePath = file.getCanonicalPath();
+        initializeLineNumberData(file);
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         JSONTokener jsonTokener = new JSONTokener(bufferedReader);
         try {
@@ -42,6 +46,24 @@ public class JSONFile {
 
     public String getFilePath() {
         return filePath;
+    }
+
+    private void initializeLineNumberData(File file) throws IOException{
+        linesInFile = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            linesInFile.add(line);
+        }
+    }
+
+    public int getLineNumber(String offendingText) {
+        for (int i = 0; i < linesInFile.size(); i++) {
+            if (linesInFile.get(i).contains(offendingText)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
